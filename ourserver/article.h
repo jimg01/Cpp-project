6a099ae1e04f9c63720abf0a1d922b751671f3b7
izzyWhizzy <isabelle.bjornlund@hotmail.com>
2024-04-12 15:35:29 +0200

    articles implemented ver1

---
+++ ourserver/article.h
@@ -0,0 +1,22 @@
+#ifndef ARTICLE_H
+#define ARTICLE_H
+
+#include <string>
+
+
+class Article{
+
+	public:
+		Article(int ind);
+		Article(int ind, std::string titl, std::string auth, std::string txt);
+		//~Article(); 	// not req due to no pointers atm
+		std::string toString();
+		
+	private:
+		std::string title, author, text;
+		int id;
+
+	
+};
+
+#endif
