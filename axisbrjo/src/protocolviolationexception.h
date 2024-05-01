52bee132f50100995e65489009703cdbf812e0fa
izzyWhizzy <isabelle.bjornlund@hotmail.com>
2024-05-01 16:03:34 +0200

    remvoed unused code in protocolviolationexception

--- axisbrjo/src/protocolviolationexception.h
+++ axisbrjo/src/protocolviolationexception.h
@@ -16,16 +16,4 @@
 #include <iostream>
 
 struct ProtocolViolationException {};
-/*
-struct ProtocolViolationException(std::string message, Protocol num, int codeRec) {
-	std::cerr << message << std::endl;
-	std::cerr << "Received " << codeRec << std::endl;
-	std::cerr << "Should be " << int(num) << std::endl;
-};
-
-struct ProtocolViolationException(std::string mess1, std::string mess2) {
-	std::cerr << mess1 << mess2 << std::endl;
-};
-*/
-//make a more flexible implementation for change of number of paramaters??
 #endif
